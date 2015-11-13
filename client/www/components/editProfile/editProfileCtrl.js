fcee43451a148b2fd6d677b09ce5908f87baf65f
Kevin Saldana <ksaldana1@yahoo.com>
2015-11-13 11:47:01 -0600

    controller functionality added. attached models to form data

--- client/www/components/editProfile/editProfileCtrl.js
+++ client/www/components/editProfile/editProfileCtrl.js
@@ -2,4 +2,11 @@ angular.module('starter')
 
 .controller('editProfileCtrl', function($scope) {
   $scope.user = {};
+  $scope.user.artist = false;
+  $scope.user.name = "";
+  $scope.user.email = "";
+  $scope.user.twitter = "";
+  $scope.user.paypal = "";
+  $scope.user.website = "";
+
 });
