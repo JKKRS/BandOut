756ce9f9f24bfb98f34a50ab5b4b4d3ce4457ff9
Scott Schwartz <sschwa12@gmail.com>
2015-11-14 11:53:24 -0600

    hopefully fix some travis stuff

--- server/apis/users-api.js
+++ server/apis/users-api.js
@@ -7,14 +7,14 @@ var UsersAPI = express.Router();
 UsersAPI.post('/', function(req, res) {
   var newUser = new User(req.body);
   newUser.save(function(err, user) {
-    if (err) { console.log('error', err); }
+    if (err) { return err }
     res.status(201).send(user);
   })
 })
 
 UsersAPI.get('/', function(req, res) {
   User.find({}, function(err, user) {
-    if (err) { console.log('error', err) }
+    if (err) { return err }
     res.status(200).send(user)
   })
 })
