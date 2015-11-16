102a6b4e8c9dbbc4c38035c212b726bf9a35a142
Kevin Saldana <ksaldana1@yahoo.com>
2015-11-16 10:57:23 -0600

    semicolons added

--- server/apis/artists-api.js
+++ server/apis/artists-api.js
@@ -7,32 +7,32 @@ var ArtistsAPI = express.Router();
 ArtistsAPI.post('/', function(req, res) {
   var newArtist = new User(req.body);
   newArtist.save(function(err, artist) {
-    if (err) { return err }
+    if (err) { return err; }
       res.status(201).send(artist);
-  })
-})
+  });
+});
 
 ArtistsAPI.get('/', function(req, res) {
   User.find({ artist : true }, function(err, artist) {
-    if (err) { return err }
+    if (err) { return err; }
       res.status(200).send(artist);
-  })
-})
+  });
+});
 
 ArtistsAPI.get('/:id', function(req, res) {
   var id = req.params.id;
   User.findOne({ fbid : id, artist : true }, function(err, artist) {
-    if (err) { return err }
+    if (err) { return err; }
     res.status(200).send(artist);
-  })
-})
+  });
+});
 
 ArtistsAPI.put('/:id', function(req, res) {
   var id = req.params.id;
   User.update( { fbid : id }, { $set : req.body }, null, function(err, msg) {
-    if (err) { return err }
-    res.status(202).send(msg)
-  })
-})
+    if (err) { return err; }
+    res.status(202).send(msg);
+  });
+});
 
 module.exports = ArtistsAPI;
