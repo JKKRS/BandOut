4b5fdaf22d2358ec7d916de073d16ae2f16fbf65
Kevin Saldana <ksaldana1@yahoo.com>
2015-11-17 12:06:47 -0600

    maps added to event view page

--- client/www/components/eventView/eventView.controller.js
+++ client/www/components/eventView/eventView.controller.js
@@ -1,7 +1,24 @@
-angular.module('starter.eventView', [])
+angular.module('starter.eventView', ['uiGmapgoogle-maps'])
 
 .controller('eventViewCtrl', eventViewCtrl);
 
 function eventViewCtrl($scope, $stateParams) {
   $scope.event = $stateParams.event;
+  // temporary lat-long. will replace with Band in Town API information
+  var myLatlng = new google.maps.LatLng(45 ,-73);
+    var mapOptions = {
+        streetViewControl: true,
+        center: myLatlng,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById('map'),
+        mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map
+    });
+    $scope.map = map;
 }
