f4c14671e0c3e41ee54d858c921a236f9edc0f38
lee25home <lee25home@gmail.com>
2015-11-17 14:04:43 -0600

    Browse opens Map and your location

--- client/www/components/browse/browse.controller.js
+++ client/www/components/browse/browse.controller.js
@@ -1,36 +1,40 @@
-angular.module('starter.mapBrowse', [])
-.controller('MapController', function($scope, $ionicLoading, $compile) {
-    //$compile - HTML string into a tempalte and produces a template functio
-    //used to link SCOPE and template together
-    google.maps.event.addDomListener(window, 'load', function() {
-        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+angular.module('starter.mapBrowse', ['uiGmapgoogle-maps'])
+  .controller('MapController', function($scope, $ionicLoading, $compile) {
+      //$compile - HTML string into a tempalte and produces a template functio
+      //used to link SCOPE and template together
+      google.maps.event.addDomListener(window, 'load', function() {
+          var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
 
-        var mapOptions = {
+          var mapOptions = {
             center: myLatlng,
             zoom: 16,
             mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+          };
 
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+          var map = new google.maps.Map(document.getElementById("map1"), mapOptions);
 
-        $scope.loading = $ionicLoading.show({
-          content:"Finding Buskers Near You",
-          showBackdrop:false
-        });
-        
+          $scope.loading = $ionicLoading.show({
+            content: "Finding Buskers Near You",
+            showBackdrop: false
+          });
 
+          $scope.centerOnMe = function() {
+            if (!$scope.map) {
+              return;
+            }
+          }
 
-        navigator.geolocation.getCurrentPosition(function(pos) {
-            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-            $scope.loading.hide();
-            var myLocation = new google.maps.Marker({
+            navigator.geolocation.getCurrentPosition(function(pos) {
+              map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+              $scope.loading.hide();
+              var myLocation = new google.maps.Marker({
                 position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
                 map: map,
                 title: "My Location"
+              });
             });
-        });
 
-        $scope.map = map;
-    });
+            $scope.map = map;
+          });
 
-});
+      });
