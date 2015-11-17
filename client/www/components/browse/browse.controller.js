a57220192064a5f1b7104a524b66c667b64bbfac
lee25home <lee25home@gmail.com>
2015-11-17 15:36:22 -0600

    Working on android and ios

--- client/www/components/browse/browse.controller.js
+++ client/www/components/browse/browse.controller.js
@@ -1,40 +1,36 @@
 angular.module('starter.mapBrowse', ['uiGmapgoogle-maps'])
-  .controller('MapController', function($scope, $ionicLoading, $compile) {
-      //$compile - HTML string into a tempalte and produces a template functio
-      //used to link SCOPE and template together
-      google.maps.event.addDomListener(window, 'load', function() {
-          var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+  .controller('MapController', function($scope, $ionicLoading) {
+    var Latlng = new google.maps.LatLng(34.045148, -118.564925);
+    var mapOptions = {
+      center: Latlng,
+      zoom: 18,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-          var mapOptions = {
-            center: myLatlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-          };
-
-          var map = new google.maps.Map(document.getElementById("map1"), mapOptions);
-
-          $scope.loading = $ionicLoading.show({
-            content: "Finding Buskers Near You",
-            showBackdrop: false
-          });
-
-          $scope.centerOnMe = function() {
-            if (!$scope.map) {
-              return;
-            }
-          }
-
-            navigator.geolocation.getCurrentPosition(function(pos) {
-              map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-              $scope.loading.hide();
-              var myLocation = new google.maps.Marker({
-                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-                map: map,
-                title: "My Location"
-              });
-            });
-
-            $scope.map = map;
-          });
-
+    $scope.center = function() {
+      navigator.geolocation.getCurrentPosition(function(pos) {
+        map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
       });
+    };
+
+    var map = new google.maps.Map(document.getElementById("map1"), mapOptions);
+
+    $scope.loading = $ionicLoading.show({
+      template: "Finding Buskers Near You",
+      showBackdrop: false
+    });
+
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      $scope.loading.hide();
+      var myLocation = new google.maps.Marker({
+        position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+        map: map,
+        title: "My Location"
+      });
+    });
+
+    $scope.map = map;
+
+
+  });
