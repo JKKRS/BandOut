589be8fe8fa053113063ce35cf882ba9b5f90c53
lee25home <lee25home@gmail.com>
2015-11-18 16:43:32 -0600

    Add Show Update

--- client/www/components/addEvent/addEvent.controller.js
+++ client/www/components/addEvent/addEvent.controller.js
@@ -1,7 +1,90 @@
-angular.module('starter.addEvent', [])
+angular.module('starter.addEvent', ['uiGmapgoogle-maps'])
 
 .controller('addEventCtrl', addEventCtrl);
 
-function addEventCtrl($scope) {
-  $scope.event = {};
+function addEventCtrl($scope, $http, User, UserService) {
+  $scope.user = {};
+  $scope.user.title = "";
+  $scope.user.venueName = "";
+  $scope.user.date = "";
+  // $scope.user.time = "";
+  $scope.user.venueAddress = "";
+  $scope.user.venueCity = "";
+  // $scope.user.venueCountry = "";
+  $scope.user.venueZip = "";
+  $scope.user.description = "";
+  $scope.location = {}
+  $scope.location.lat = null;
+  $scope.location.long = null;
+  // var lat = null;
+  // var long = null;
+
+  var geocoder = new google.maps.Geocoder();
+
+  // var address = "1200 Getty Center Dr"
+  $scope.saveEvent = function() {
+
+    geocoder.geocode({
+      'address': $scope.user.venueAddress + ' ' + $scope.user.venueCity + ' ' + $scope.user.venueZip
+    }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        $scope.location.lat = results[0].geometry.location.lat();
+        $scope.location.long = results[0].geometry.location.lng();
+        console.log('Hello', $scope.location.lat);
+      } else {
+        $scope.location.lat = 34.0775;
+        $scope.location.long = 118.4750;
+      }
+      var venueAdd = NewVenue(
+        $scope.user.venueName,
+        $scope.user.venueCity,
+        $scope.location.lat,
+        $scope.location.long
+      );
+      console.log("One", venueAdd);
+
+      var venueId = Math.floor(Math.random() * 82) + 'BO';
+      console.log(venueId);
+
+      var eventAdd = NewEvent(
+        venueId,
+        $scope.user.title,
+        $scope.user.date,
+        $scope.user.description,
+        venueAdd
+      );
+      console.log("two", eventAdd);
+
+      User.update({
+        "fbid": $scope.user.fbid
+      }, eventAdd);
+
+    });
+
+
+  };
+}
+
+function NewEvent(id, title, datetime, description, venue) {
+  var newEvent = Object.create(Object.prototype);
+  console.log('Check', venue);
+  newEvent = {
+    "id": id,
+    "title": title,
+    "datetime": datetime,
+    "description": description,
+    "venue": venue
+  };
+  return newEvent;
+}
+
+function NewVenue(name, city, latit, longit) {
+  var newVenue = Object.create(Object.prototype);
+  newVenue = {
+    "name": name,
+    "city": city,
+    "latitude": latit,
+    "longitude": longit
+  };
+  return newVenue;
 }
