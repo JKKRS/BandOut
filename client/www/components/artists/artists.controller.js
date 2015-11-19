5ce4a82a3b2021007e5d2ff91aec1ee5919c1d64
lee25home <lee25home@gmail.com>
2015-11-19 11:02:53 -0600

    Add swipe left to navigate to next state

--- client/www/components/artists/artists.controller.js
+++ client/www/components/artists/artists.controller.js
@@ -6,12 +6,19 @@ function ArtistsCtrl($scope, $state, Artist) {
   $scope.artists = Artist.query();
   $scope.refresh = function() {
     Artist.query().$promise.then(function(allArtists) {
-       // Stop the ion-refresher from spinning
-       $scope.artists = allArtists;
-       $scope.$broadcast('scroll.refreshComplete');
+      // Stop the ion-refresher from spinning
+      $scope.artists = allArtists;
+      $scope.$broadcast('scroll.refreshComplete');
     });
   };
   $scope.artistDetail = function(artist) {
+    $state.go('app.artists.artist', {
+      artist: artist,
+      artistId: artist.fbid
+    });
+  };
+
+  $scope.onSwipeLeft = function(artist) {
     $state.go('app.artists.artist', {
       artist: artist,
       artistId: artist.fbid
