34daf69bc744a479c4bebf26b6521c4621f42c28
lee25home <lee25home@gmail.com>
2015-11-19 17:19:37 -0600

    Workiing edit

--- client/www/components/editEventView/editEventView.controller.js
+++ client/www/components/editEventView/editEventView.controller.js
@@ -1,18 +1,72 @@
 angular.module('starter.editEventView', ['uiGmapgoogle-maps'])
   .controller('editEventViewCtrl', editEventViewCtrl);
 
-function editEventViewCtrl($scope, $state, User, UserService, $stateParams) {
+function editEventViewCtrl($scope, $state, User, $stateParams, UserService) {
   $scope.event = $state.params.event;
+  $scope.location = {};
+  $scope.location.lat = null;
+  $scope.location.long = null;
 
+  var geocoder = new google.maps.Geocoder();
   $scope.updateEvent = function() {
-    console.log("I will be functional");
+    geocoder.geocode({
+      'address': $scope.event.venue.address + ' ' + $scope.event.venue.city + ' ' + $scope.event.venue.zip
+    }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        $scope.location.lat = results[0].geometry.location.lat();
+        $scope.location.long = results[0].geometry.location.lng();
+      } else {
+        $scope.location.lat = 34.0775;
+        $scope.location.long = 118.4750;
+      }
+      var venueAdd = NewVenue(
+        $scope.event.venue.name,
+        $scope.event.venue.address,
+        $scope.event.venue.zip,
+        $scope.event.venue.city,
+        $scope.location.lat,
+        $scope.location.long
+      );
+
+      var eventAdd = NewEvent(
+        $scope.event.title,
+        $scope.event.datetime,
+        $scope.event.description,
+        venueAdd
+      );
+
+      UserService.getUser()
+        .then(function(res){
+          var updatedShow = res.artist_info.upcoming_events;
+          for(var i= 0; i< updatedShow.lenght; i++){
+            if(updatedShow[i].id === $scope.event.id){
+              updatedShow[i] = eventAdd;
+            } else {
+              return updatedShow;
+            }
+          }
+
+          var userObj = {
+            artist_info: {
+              paypal_link: res.artist_info.paypal_link,
+              upcoming_events: []
+            }
+          };
+
+          userObj.artist_info.upcoming_events = updatedShow;
+          User.update({
+            "fbid":res.fbid
+          }, userObj);
+
+        });
+
+    });
   };
 }
 
-function NewEvent(id, title, datetime, description, venue) {
+function NewEvent( title, datetime, description, venue) {
   var newEvent = Object.create(Object.prototype);
   newEvent = {
-    "id": id,
     "title": title,
     "datetime": datetime,
     "description": description,
