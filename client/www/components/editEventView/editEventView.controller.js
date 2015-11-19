5b779b7db6bb4a039a4b3350769d2b5682abed9d
lee25home <lee25home@gmail.com>
2015-11-19 15:28:09 -0600

    "Retracing to start fresh"

--- client/www/components/editEventView/editEventView.controller.js
+++ client/www/components/editEventView/editEventView.controller.js
@@ -1,14 +1,36 @@
-angular.module('starter.editEventView', [])
+angular.module('starter.editEventView', ['uiGmapgoogle-maps'])
   .controller('editEventViewCtrl', editEventViewCtrl);
 
-function editEventViewCtrl($scope, $state, $stateParams) {
-  $scope.event = $stateParams.event;
-  $scope.event.title = "";
-  $scope.user.venueName ="";
-  $scope.user.date = null;
-  // $scope.user.time = null
-  // $scope.user.venueAddress = 
-  // $scope.updateEvent = function() {
-  //
-  // };
+function editEventViewCtrl($scope, $state, User, UserService, $stateParams) {
+  $scope.event = $state.params.event;
+  
+  $scope.updateEvent = function() {
+    console.log("I will be functional");
+  };
+}
+
+function NewEvent(id, title, datetime, description, venue) {
+  var newEvent = Object.create(Object.prototype);
+  newEvent = {
+    "id": id,
+    "title": title,
+    "datetime": datetime,
+    "description": description,
+    "venue": venue
+  };
+  return newEvent;
+}
+
+function NewVenue(name, address, zip, city, latit, longit) {
+  var newVenue = Object.create(Object.prototype);
+  newVenue = {
+    "name": name,
+    "address": address,
+    "zip": zip,
+    "city": city,
+    "country": "",
+    "latitude": latit,
+    "longitude": longit
+  };
+  return newVenue;
 }
