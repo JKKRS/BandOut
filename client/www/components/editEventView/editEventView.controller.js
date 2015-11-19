f2f157e4098fb6d2c5c1898d80c67e5fcaa19813
lee25home <lee25home@gmail.com>
2015-11-19 14:03:27 -0600

    Working on editable fields

--- client/www/components/editEventView/editEventView.controller.js
+++ client/www/components/editEventView/editEventView.controller.js
@@ -1,6 +1,14 @@
 angular.module('starter.editEventView', [])
-.controller('editEventViewCtrl', editEventViewCtrl);
+  .controller('editEventViewCtrl', editEventViewCtrl);
 
-function editEventViewCtrl ($scope, $state, $stateParams) {
+function editEventViewCtrl($scope, $state, $stateParams) {
   $scope.event = $stateParams.event;
+  $scope.event.title = "";
+  $scope.user.venueName ="";
+  $scope.user.date = null;
+  // $scope.user.time = null
+  // $scope.user.venueAddress = 
+  // $scope.updateEvent = function() {
+  //
+  // };
 }
