3ba1a988f37c6fa552fcf9dfcfdbab6f8e62a841
Kevin Saldana <ksaldana1@yahoo.com>
2015-11-19 14:25:28 -0600

    added pull to refresh to individudal artist page

--- client/www/components/artist/artist.controller.js
+++ client/www/components/artist/artist.controller.js
@@ -2,11 +2,18 @@ angular.module('starter.artist', ['starter.services'])
 
 .controller('ArtistCtrl', ArtistCtrl);
 
-function ArtistCtrl($scope, $stateParams, $window, $state, Artist) {
+function ArtistCtrl($scope, $stateParams, $window, $state, User, Artist, $resource) {
   $scope.artist = $stateParams.artist;
   $scope.payPal = function(link) {
     link = link.toString();
       $window.open( link, '_blank', 'location=yes');
+  };
+  $scope.refresh = function() {
+    Artist.get({'artistId': $scope.artist.fbid})
+    .$promise.then(function(data) {
+      $scope.artist = data;
+      $scope.$broadcast('scroll.refreshComplete');
+    });
   };
   $scope.eventDetail = function(event) {
     $state.go('app.artists.artist-event', {event: event, eventId: event.id});
