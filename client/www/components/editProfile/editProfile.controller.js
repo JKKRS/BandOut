7f32d68b3d886a36882408f682c6baa23e9894e2
Scott Schwartz <sschwa12@gmail.com>
2015-11-19 14:21:30 -0600

    paypal validation complete
    Closes #118 -- I left the label as-is since using the first part of the URL as the label looked a little wonky. Probably will want to come up with a different UI design than using placeholders.

--- client/www/components/editProfile/editProfile.controller.js
+++ client/www/components/editProfile/editProfile.controller.js
@@ -13,13 +13,13 @@ function editProfileCtrl($scope, $state, User, UserService, $timeout) {
   $scope.user.website = "";
   $scope.user.fbid = "";
 
-  $scope.$watchGroup(['user.paypal', 'user.twitter', 'user.website', 'user.artist'], function(newVal, oldVal, scope) {
-      if ($scope.initialLoad) {
-        $scope.initialLoad = false;
-      } else {
-        $scope.disableSave = false;
-      }
-  });
+  // $scope.$watchGroup(['user.paypal', 'user.twitter', 'user.website', 'user.artist'], function(newVal, oldVal, scope) {
+  //     if ($scope.initialLoad) {
+  //       $scope.initialLoad = false;
+  //     } else {
+  //       $scope.disableSave = false;
+  //     }
+  // });
 
   $scope.create = function() {
     $state.go('app.editProfile.addEvent', {user: $scope.user});
@@ -29,27 +29,40 @@ function editProfileCtrl($scope, $state, User, UserService, $timeout) {
   $scope.retrieveUser = function() {
     UserService.getUser()
     .then(function(res) {
-      // console.log('called', res);
+      // Strip the beginning of the paypal URL
+      var url = res.artist_info.paypal_link;
+      var index = url.lastIndexOf('/');
+      var pp_id = url.substr(index + 1)
+
       $scope.user.name = res.name;
       $scope.user.email = res.email;
       $scope.user.twitter = res.twitter;
       $scope.user.artist = res.artist;
-      $scope.user.paypal = res.artist_info.paypal_link;
+      $scope.user.paypal = pp_id;
       $scope.user.website = res.artist_info.website;
       $scope.user.fbid = res.fbid;
     }).then(function() {
-      $scope.disableSave = true;
-      $scope.initialLoad = true;
+      // $scope.disableSave = true;
+      // $scope.initialLoad = true;
     });
   };
+
+  // not currently being used, but can be used to validate on field update / change
+  // $scope.validate = function() {
+  //   if (/^[a-zA-Z0-9]+$/.test($scope.user.paypal) === false) {
+  //     $scope.disableSave = true;
+  //   }
+  // }
 
   // Create new user from scope vars, and submit PUT request to server using fbid
   // to search for user in db
   $scope.saveUser = function() {
+    // Append url to paypal ID
+    var paypal = 'https://paypal.me/' + $scope.user.paypal;
     var user = NewUser(
         $scope.user.artist,
         $scope.user.twitter,
-        $scope.user.paypal,
+        paypal,
         $scope.user.website
       );
     User.update({ "fbid" : $scope.user.fbid }, user);
