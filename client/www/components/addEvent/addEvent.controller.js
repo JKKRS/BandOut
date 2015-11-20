1f701feb6ec5873d27cf7c0a68e67d54bcabb086
Scott Schwartz <sschwa12@gmail.com>
2015-11-19 20:17:15 -0600

    timestamp

--- client/www/components/addEvent/addEvent.controller.js
+++ client/www/components/addEvent/addEvent.controller.js
@@ -8,29 +8,51 @@ function addEventCtrl($scope, User, $stateParams, $cordovaDatePicker, UserServic
   $scope.location.lat = null;
   $scope.location.long = null;
 
-  var datePickerOptions = {
+  $scope.datePicker = function(e) {
+    $cordovaDatePicker.show().then(function(date){
+      $scope.user.date = date;
+    });
+  }
+
+  var timeOptions = {
     date: new Date(),
-    mode: 'date', // or 'time'
-    minDate: new Date() - 10000,
+    mode: 'time', // or 'time'
+    minDate: new Date(),
     allowOldDates: true,
     allowFutureDates: true,
     doneButtonLabel: 'DONE',
-    doneButtonColor: '#000000',
+    // doneButtonColor: '#000000',
     cancelButtonLabel: 'CANCEL',
     cancelButtonColor: '#000000'
-  };
+  }
 
-  $scope.datePicker = function() {
-    $cordovaDatePicker.show(datePickerOptions).then(function(date){
-        console.log(date);
-    });
+  $scope.timePicker = function(e) {
+    $cordovaDatePicker.show(timeOptions).then(function(time) {
+      $scope.user.time = time;
+    })
   }
 
   var geocoder = new google.maps.Geocoder();
   $scope.saveEvent = function() {
     geocoder.geocode({
       'address': $scope.user.venueAddress + ' ' + $scope.user.venueCity + ' ' + $scope.user.venueZip
-    }, function(results, status) {
+    },
+    function(results, status) {
+
+      console.log('date', $scope.user.date, 'time', $scope.user.time);
+
+      var date = moment($scope.user.date);
+      var time = moment($scope.user.time);
+      var dateString = date.format('YYYY-MM-DD');
+      var timeString = time.format('hh:mm:ss.SSS');
+
+      var timestamp = dateString + 'T' + timeString + 'Z'
+      console.log('timestamp', timestamp);
+      // console.log('new date', date)
+      // console.log('new time', time);
+
+
+
       if (status == google.maps.GeocoderStatus.OK) {
         $scope.location.lat = results[0].geometry.location.lat();
         $scope.location.long = results[0].geometry.location.lng();
@@ -38,6 +60,7 @@ function addEventCtrl($scope, User, $stateParams, $cordovaDatePicker, UserServic
         $scope.location.lat = 34.0775;
         $scope.location.long = 118.4750;
       }
+
       var venueAdd = NewVenue(
         $scope.user.venueName,
         $scope.user.venueAddress,
@@ -46,6 +69,7 @@ function addEventCtrl($scope, User, $stateParams, $cordovaDatePicker, UserServic
         $scope.location.lat,
         $scope.location.long
       );
+
       var venueId = Math.floor(Math.random() * 832);
 
       var eventAdd = NewEvent(
