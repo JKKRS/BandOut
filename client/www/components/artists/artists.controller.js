93feceb44f6067444f20c781389ede75cea16586
James Hicks <jehicks2@yahoo.com>
2015-11-20 16:16:06 -0600

    Paypal on Artists page links out

--- client/www/components/artists/artists.controller.js
+++ client/www/components/artists/artists.controller.js
@@ -1,9 +1,15 @@
-angular.module('starter.artists', ['starter.services'])
+angular.module('starter.artists', ['starter.services', 'starter.artist'])
 
 .controller('ArtistsCtrl', ArtistsCtrl);
 
-function ArtistsCtrl($scope, $state, Artist) {
+function ArtistsCtrl($scope, $stateParams, $window, $state, Artist) {
   $scope.artists = Artist.query();
+
+  $scope.payPal = function(link) {
+    link = link.toString();
+      $window.open( link, '_blank', 'location=yes');
+  };
+
   $scope.refresh = function() {
     Artist.query().$promise.then(function(allArtists) {
       // Stop the ion-refresher from spinning
