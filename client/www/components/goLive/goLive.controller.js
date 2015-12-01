6dacfd51d5c8423f816cc019e68a55aea72dbc9f
Kevin Saldana <ksaldana1@yahoo.com>
2015-12-01 10:40:34 -0600

    go live click function added

--- client/www/components/goLive/goLive.controller.js
+++ client/www/components/goLive/goLive.controller.js
@@ -1,5 +1,21 @@
 angular.module('starter.goLive', [])
 
-.controller('GoLiveCtrl', function($scope) {
-  $scope.user = "";
+.controller('GoLiveCtrl', function($scope, store, User, $cordovaGeolocation) {
+  $scope.artistLive = function() {
+    $cordovaGeolocation.getCurrentPosition({
+      timeout: 10000,
+      enableHighAccuracy: false
+    }).then(function(pos) {
+      $scope.fbid = store.get('profile').user_id.substr(9);
+      $scope.coords = [pos.coords.longitude, pos.coords.latitude];
+      User.update({
+        'fbid': $scope.fbid
+      }, {
+        live: true,
+        location: {
+          coordinates: $scope.coords
+        }
+      }).$promise.then(function() { console.log('user saved'); });
+    });
+  };
 });
