77b183dc8c0b8f3f6382aa2b67741d4c252ed472
ksaldana1 <ksaldana1@gmail.com>
2015-12-07 10:07:24 -0600

    added comments - artist controller

--- client/www/components/artist/artist.controller.js
+++ client/www/components/artist/artist.controller.js
@@ -3,11 +3,14 @@ angular.module('starter.artist', ['starter.services'])
 .controller('ArtistCtrl', ArtistCtrl);
 
 function ArtistCtrl($scope, $stateParams, $window, $state, Artist) {
+  // set artist to artist object passed from artists view
   $scope.artist = $stateParams.artist;
+  // on-click handler for paypal information
   $scope.payPal = function(link) {
     link = link.toString();
       $window.open( link, '_blank', 'location=yes');
   };
+  // pull to refresh function to display newly updated information
   $scope.refresh = function() {
     Artist.get({'artistId': $scope.artist.fbid})
     .$promise.then(function(data) {
@@ -15,10 +18,11 @@ function ArtistCtrl($scope, $stateParams, $window, $state, Artist) {
       $scope.$broadcast('scroll.refreshComplete');
     });
   };
+  // click handler for individual event view. pass event information as stateParam
   $scope.eventDetail = function(event) {
     $state.go('app.artists.artist-event', {event: event, eventId: event.id});
   };
-
+  // gesture functionality --> same as $scope.eventDetail
   $scope.onSwipeLeft =function(event) {
     $state.go('app.artists.artist-event', {event: event, eventId: event.id});
   };
