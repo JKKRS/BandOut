96823bd4bd38a128202515ca2e7a7af0cfd3aeda
ksaldana1 <ksaldana1@gmail.com>
2015-12-07 15:44:58 -0600

    fixed go live button functionality

--- client/www/components/goLive/goLive.controller.js
+++ client/www/components/goLive/goLive.controller.js
@@ -1,5 +1,5 @@
 angular.module('main.goLive', [])
-.controller('GoLiveCtrl', GoLiveCtrl);
+  .controller('GoLiveCtrl', GoLiveCtrl);
 
 function GoLiveCtrl($scope, store, User, $cordovaGeolocation, $ionicModal, $ionicLoading) {
   // check if current user is artist. ng-show functionality
@@ -10,47 +10,40 @@ function GoLiveCtrl($scope, store, User, $cordovaGeolocation, $ionicModal, $ioni
   $scope.liveButton = false;
 
   // click handler --> artist goes live
-  // I'm not sure this works. Need to refactor
   $scope.goLive = function() {
     $scope.liveButton = !$scope.liveButton;
     if ($scope.liveButton) {
-      // function assignment, but is this actually firing?
-      $scope.artistLive = function() {
-        $ionicLoading.show();
-        $cordovaGeolocation.getCurrentPosition({
-          timeout: 10000,
-          enableHighAccuracy: false
-        }).then(function(pos) {
-          $scope.fbid = store.get('profile').user_id;
-          $scope.coords = [pos.coords.longitude, pos.coords.latitude];
-          console.log($scope.coords);
-          User.update({
-            'fbid': $scope.fbid
-          }, {
-            live: true,
-            'location.coordinates': $scope.coords
-          });
-          // .$promise.then(function() {
-          // $ionicLoading.hide();
-          // $scope.openModal();
-          // });
-        });
-      };
-    } else {
-      // click handler --> artist stops broadcasting live
-      // same as above. Is this actually firing?
-      $scope.stopEvent = function() {
+      $ionicLoading.show();
+      $cordovaGeolocation.getCurrentPosition({
+        timeout: 10000,
+        enableHighAccuracy: false
+      }).then(function(pos) {
         $scope.fbid = store.get('profile').user_id;
+        $scope.coords = [pos.coords.longitude, pos.coords.latitude];
+        console.log($scope.coords);
         User.update({
           'fbid': $scope.fbid
         }, {
-          live: false,
-          'location.coordinates': [0, 0]
+          live: true,
+          'location.coordinates': $scope.coords
         });
         // .$promise.then(function() {
-        // $scope.closeModal();
-        //  });
-      };
+        // $ionicLoading.hide();
+        // $scope.openModal();
+        // });
+      });
+    } else {
+      // click handler --> artist stops broadcasting live
+      $scope.fbid = store.get('profile').user_id;
+      User.update({
+        'fbid': $scope.fbid
+      }, {
+        live: false,
+        'location.coordinates': [0, 0]
+      });
+      // .$promise.then(function() {
+      // $scope.closeModal();
+      //  });
     }
   };
-};
+}
