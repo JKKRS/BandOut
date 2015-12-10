1789c6cf315f1b9b35a7482ab27efecb50e00d86
James Hicks <jehicks2@yahoo.com>
2015-12-10 11:26:03 -0600

    Arrow in InfoWindow

--- client/www/components/nearbyArtists/nearbyArtists.controller.js
+++ client/www/components/nearbyArtists/nearbyArtists.controller.js
@@ -176,12 +176,12 @@ function NearbyArtistsController($scope, $stateParams, $window, $timeout, $ionic
       // Moves the arrow 76px to the left margin
       iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'right: 90px !important;';});
 
-      // iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
+      iwBackground.children(':nth-child(3)').find('div').children().css({'background-color' : '#3a3a3a'});
       var iwCloseBtn = iwOuter.next();
       // Apply the desired effect to the close button
       iwCloseBtn.css({'display' : 'none'});
     });
-
+// 'box-shadow': 'rgba(255, 76, 10 1) 0px 1px 6px', 'z-index' : '1'
       // marker.addListener('click', function() {
       //   infowindow.open(targetMap, marker);
       // });
