4f2cda1c0dad476e64c7c62fded97e7211493dcf
ksaldana1 <ksaldana1@gmail.com>
2016-02-17 11:43:58 -0600

    Removed more controller code in artists component. Note: favorite directive needs refactor

--- client/www/components/artistListItem/artistList.js
+++ client/www/components/artistListItem/artistList.js
@@ -5,6 +5,7 @@ angular.module('main.artistList', ['main.services'])
     scope: {},
     bindToController: {
       artists: '=',
+      obj: '='
     },
     controller: ArtistListItemCtrl,
     controllerAs: 'listItem',
@@ -13,7 +14,9 @@ angular.module('main.artistList', ['main.services'])
 });
 
 
-function ArtistListItemCtrl($window, $state) {
+function ArtistListItemCtrl($window, $state, store) {
+  this.user = store.get('userData');
+
   this.payPal = function(link) {
     link = link.toString();
     $window.open(link, '_blank', 'location=yes');
@@ -26,6 +29,44 @@ function ArtistListItemCtrl($window, $state) {
       artistId: artist.fbid
     });
   };
+
+  this.getClass = artist => {
+    return {
+      'ion-ios-heart': this.user.favorite_artists.indexOf(artist.fbid) > -1,
+      'ion-ios-heart-outline': this.user.favorite_artists.indexOf(artist.fbid) === -1
+    };
+  };
+
+  this.favorite = function(artist) {
+    console.log('favorite fired');
+    var userID = store.get('profile').user_id;
+    User.get({
+      'fbid': userID
+    }, function(currentUser) {
+      var favs = currentUser.favorite_artists || [];
+      if (!_.contains(favs, artist.fbid)) {
+        favs.push(artist.fbid);
+      } else {
+        favs = favs.filter(function(id) {
+          return id !== artist.fbid;
+        });
+      }
+      currentUser.favorite_artists = favs;
+      User.update({
+        'fbid': userID
+      }, currentUser);
+    });
+  };
+
+
+
+  // function(artist) {
+  //   console.log('get class fired');
+  //   return {
+  //     'ion-ios-heart': $scope.user.favorite_artists.indexOf(artist.fbid) > -1,
+  //     'ion-ios-heart-outline': $scope.user.favorite_artists.indexOf(artist.fbid) === -1
+  //   };
+  // };
 
 }
 
